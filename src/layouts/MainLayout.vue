6afc4ac303769b31f87bfadf95e4d81ed8e1698a
Andrew <npth@yandex.ru>
2025-03-28 14:36:16 +0300

    элементы фильтра карты

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,40 +2,29 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item clickable to="/imap">
+          <q-item-section avatar>
+            <q-icon name="map" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Interactive Map</q-item-label>
+            <q-item-label caption>Interactive Map</q-item-label>
+          </q-item-section>
+        </q-item>
+
       </q-list>
     </q-drawer>
 
@@ -47,56 +36,10 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
---
+++ src/pages/InteractiveMap.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <div class="text-h3 col-12 text-bold">Interactive map</div>
+
+    <q-card flat bordered style="min-height: 150px;">
+      <q-card-section>
+        <div class="text-h6 text-bold">Select</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions vertical>
+
+        <q-select class="q-mb-md" v-model="model_select1" dense square outlined clearable :options="options" color="primary" label="Отрасли предприятия" />
+
+        <q-select class="q-mb-md"  v-model="model_select2" dense square outlined clearable :options="options" color="primary" label="Отрасли предприятия" />
+
+        <q-select v-model="model_select1" dense square outlined disable clearable :options="options" label="Отрасли предприятия" />
+
+      </q-card-actions>
+    </q-card>
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6 text-bold">Input</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-actions vertical>
+
+        <q-input class="q-mb-md" v-model="model_input1" dense square outlined clearable color="primary" label="Начать поиск на карте ">
+          <template v-slot:append>
+            <q-icon name="search"/>
+          </template>
+        </q-input>
+        <q-input class="q-mb-md" v-model="model_input2" dense square outlined clearable color="primary" label="Начать поиск на карте ">
+          <template v-slot:append>
+            <q-icon name="search"/>
+          </template>
+        </q-input>
+        <q-input class="q-mb-md" v-model="model_input1" dense square outlined clearable disable color="primary" label="Начать поиск на карте ">
+          <template v-slot:append>
+            <q-icon name="search"/>
+          </template>
+        </q-input>
+
+        <div class="">Потребность в инвестициях</div>
+        <div class="row">
+          <q-input dense square outlined v-model="invest_from" type="number" label="от 0 руб" class="q-mr-sm"></q-input>
+          <q-input dense square outlined v-model="invest_to" type="number" label="до 900 000 000 руб"></q-input>
+        </div>
+
+        <div class="q-mt-md">Потребность в инвестициях</div>
+        <div class="row">
+          <q-input dense square outlined v-model="invest_from1" type="number" label="от 0 руб" class="q-mr-sm"></q-input>
+          <q-input dense square outlined v-model="invest_to1" type="number" label="до 900 000 000 руб"></q-input>
+        </div>
+
+      </q-card-actions>
+    </q-card>
+
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6 text-bold">Button</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions vertical>
+        <q-btn flat square color="primary" no-caps>
+          <div style="border-bottom: solid #1553A3 1px;">
+            <span>Открыть все районы</span>
+            <q-icon name="south_east" size="1.1em"></q-icon>
+        </div></q-btn>
+        <q-btn flat square color="accent" no-caps>
+          <div style="border-bottom: solid #0A1A3E 1px;">
+            <span>Открыть все районы</span>
+            <q-icon name="south_east" size="1.1em"></q-icon>
+          </div></q-btn>
+        <q-btn flat square color="primary" no-caps disabled>
+          <div style="border-bottom: solid #0A1A3E 1px;">
+            <span>Открыть все районы</span>
+            <q-icon name="south_east" size="1.1em"></q-icon>
+          </div></q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6 text-bold">Chips</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions vertical>
+        <q-chip  square color="primary" text-color="white">Работает</q-chip>
+        <q-chip outline square color="primary" >Создаётся</q-chip>
+        <q-chip outline square color="primary" >Планируется</q-chip>
+      </q-card-actions>
+    </q-card>
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6 text-bold">Checkbox</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions vertical>
+        <q-item tag="label" dense>
+          <q-item-section>
+            <q-item-label>Предприятия</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-checkbox color="primary" keep-color v-model="checkbox1"/>
+          </q-item-section>
+        </q-item>
+        <q-item tag="label" dense>
+          <q-item-section>
+            <q-item-label>Инвестиционные предложения</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-checkbox color="herbal" keep-color v-model="checkbox2"/>
+          </q-item-section>
+        </q-item>
+        <q-item tag="label" dense disable>
+          <q-item-section>
+            <q-item-label>Инвестиционные площадки</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-checkbox color="accent" keep-color v-model="checkbox3"/>
+          </q-item-section>
+        </q-item>
+        <q-item tag="label" dense>
+          <q-item-section>
+            <q-item-label>Промышленные парки и технологии</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-checkbox color="lilac" keep-color v-model="checkbox4" />
+          </q-item-section>
+        </q-item>
+        <q-item tag="label">Муниципалиет</q-item>
+          <q-checkbox color="accent" keep-color v-model="checkbox_reg1" label="Абинский район" class="q-ml-sm"/>
+          <q-checkbox color="accent" keep-color v-model="checkbox_reg2" label="Анапа" class="q-ml-sm"/>
+          <q-checkbox color="accent" keep-color v-model="checkbox_reg3" label="Апшеронский район" class="q-ml-sm"/>
+
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+defineComponent({
+  name: 'InteractiveMap',
+});
+
+const options = ['Производство кожи, одежды, обуви и так далее', 'Производство текстиля']
+
+const model_select1 = ref(null)
+const model_select2 = ref('Производство текстиля')
+
+const model_input1 = ref(null)
+const model_input2 = ref('Промышленный парк')
+
+const checkbox1 = ref( false)
+const checkbox2 = ref( true)
+const checkbox3 = ref( false)
+const checkbox4 = ref( true)
+
+const checkbox_reg1 = ref( false)
+const checkbox_reg2 = ref( true)
+const checkbox_reg3 = ref( false)
+
+const invest_from = ref( null)
+const invest_to = ref( null)
+const invest_from1 = ref( 1000000)
+const invest_to1 = ref( 5000657)
+</script>
